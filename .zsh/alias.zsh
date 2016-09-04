c035ab8d9ecc02e1d63e6734b3264a3c71fc8f54
munisystem <munisystem@gmail.com>
2016-09-04 23:48:08 +0900

    Add git alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -4,6 +4,7 @@
 alias ls="ls -G"
 
 ## git
+alias gi='git init'
 alias ga='git add'
 alias gc='git commit'
 alias gp='git push'
@@ -12,3 +13,4 @@ alias gco='git checkout'
 alias gr='git rebase'
 alias gre='git reset'
 alias gref='git reflog'
+alias gs='git status'
