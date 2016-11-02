b57fc25377d5657177795a35caf67244efe5fa54
munisystem <munisystem@gmail.com>
2016-11-02 23:32:21 +0900

    Use mitamae

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 os=$(uname | tr "[A-Z]" "[a-z]")
 
+dotfiles=$HOME/dotfiles
+if [ -d "$dotfiles" ]; then
+  echo "👍  Skip install remote dotfiles."
+else
+  git clone git@github.com:munisystem/dotfiles "$dotfiles"
+fi
+
+cd $dotfiles
 bin/itamae local itamae/roles/$os/default.rb
