48b6fa61d056401976917fa13c1eb94e1a97f752
Yuichi Saito <munisystem@gmail.com>
2016-12-21 21:38:55 +0900

    Update zplug

--- config/.zsh/plugin.zsh
+++ config/.zsh/plugin.zsh
@@ -4,9 +4,9 @@ source $ZPLUG_HOME/init.zsh
 
 zplug "rupa/z", use:z.sh
 
-zplug 'zsh-users/zsh-autosuggestions', nice:10
+zplug 'zsh-users/zsh-autosuggestions', defer:1
 zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-syntax-highlighting", nice:18
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 zplug "mafredri/zsh-async", "on:sindresorhus/pure"
 zplug "sindresorhus/pure"
