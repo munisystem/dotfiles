27ac32776f74f41b2a74bee52c5f9ba58e629bf8
Yuichi Saito <munisystem@gmail.com>
2017-03-09 14:06:24 +0900

    Fix broken zsh keybind!!!!!!!

--- config/.zsh/keybind.zsh
+++ config/.zsh/keybind.zsh
@@ -8,6 +8,3 @@ bindkey '^N' history-search-forward
 bindkey '^R' __peco_select_history
 bindkey '^]''r' __peco_ghq
 bindkey '^]''s' __peco_sbox
-
-## zsh-autosuggest
-bindkey '^F' autosuggest-accept
