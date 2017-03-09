bc92118a4f535bb6c0154aebcaa9daaf210fde3e
Yuichi Saito <munisystem@gmail.com>
2017-03-09 13:58:57 +0900

    Fix not accept autosuggest

--- config/.zsh/keybind.zsh
+++ config/.zsh/keybind.zsh
@@ -8,3 +8,6 @@ bindkey '^N' history-search-forward
 bindkey '^R' __peco_select_history
 bindkey '^]''r' __peco_ghq
 bindkey '^]''s' __peco_sbox
+
+## zsh-autosuggest
+bindkey '^F' autosuggest-accept
