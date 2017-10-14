28aa1a1d7001c546773cc4f20f26c254e5f80ddb
Yuichi Saito <munisystem@gmail.com>
2017-10-14 15:48:43 +0900

    Remove noused plugin

--- config/.zsh/plugin.zsh
+++ config/.zsh/plugin.zsh
@@ -2,8 +2,6 @@
 export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 
-zplug "rupa/z", use:z.sh
-
 zplug 'zsh-users/zsh-autosuggestions', defer:1
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
