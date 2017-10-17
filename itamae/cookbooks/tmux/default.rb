a47abb85ed98abe2fd70402521e1cafe84c9169e
Yuichi Saito <munisystem@gmail.com>
2017-10-17 15:04:22 +0900

    Install tpm

--- itamae/cookbooks/tmux/default.rb
+++ itamae/cookbooks/tmux/default.rb
@@ -1,3 +1,12 @@
 include_recipe '../dotfiles/default.rb'
 
+git "#{ENV["HOME"]}/.tmux/plugins/tpm" do
+  repository 'https://github.com/tmux-plugins/tpm'
+end
+
+execute "#{ENV["HOME"]}/.tmux/plugins/tpm/bin/install_plugins" do
+  subscribes :run, "link[#{ENV['HOME']}/.tmux.conf]"
+  action :nothing
+end
+
 dotfiles '.tmux.conf'
