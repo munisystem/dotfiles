a3ee54a7bc510441149d44872c3048179911555b
Yuichi Saito <munisystem@gmail.com>
2022-07-25 18:59:53 +0900

    Tweak

--- itamae/roles/darwin/default.rb
+++ itamae/roles/darwin/default.rb
@@ -1,11 +1,6 @@
 include_recipe '../../lib/utils'
 
-include_cookbook 'vim'
-include_cookbook 'vimperator'
 include_cookbook 'git'
 include_cookbook 'zsh'
 include_cookbook 'nvim'
-include_cookbook 'idea'
 include_cookbook 'tmux'
-# include_cookbook 'karabiner'
-# include_cookbook 'fish'
