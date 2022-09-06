927749e8b20f7459faf7fe4217b166ccc5cddd67
Yuichi Saito <munisystem@gmail.com>
2022-09-06 12:25:53 +0900

    Tweak

--- itamae/cookbooks/tmux/default.rb
+++ itamae/cookbooks/tmux/default.rb
@@ -1,12 +1,12 @@
 include_recipe '../dotfiles/default.rb'
 
+dotfiles '.config/tmux'
+
 git "#{ENV["HOME"]}/.tmux/plugins/tpm" do
   repository 'https://github.com/tmux-plugins/tpm'
 end
 
 execute "#{ENV["HOME"]}/.tmux/plugins/tpm/bin/install_plugins" do
-  subscribes :run, "link[#{ENV['HOME']}/.tmux.conf]"
+  subscribes :run, "link[#{ENV['HOME']}/.config/tmux/tmux.conf]"
   action :nothing
 end
-
-dotfiles '.config/tmux'
