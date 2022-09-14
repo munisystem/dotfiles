5e24028e9d4f247bf73f0a2a294d1ee826bdd81d
Yuichi Saito <munisystem@gmail.com>
2022-09-14 17:47:48 +0900

    Configure expanding completion

--- config/.config/nvim/plugin/coc.lua
+++ config/.config/nvim/plugin/coc.lua
@@ -17,10 +17,9 @@ vim.cmd[[
   autocmd VimEnter * hi CocMenuSel guibg=#274964
   autocmd CursorHold * silent call CocActionAsync('highlight')
 
-  inoremap <silent><expr> <TAB>
-      \ coc#pum#visible() ? coc#pum#next(1) :
-      \ CheckBackspace() ? "\<Tab>" :
-      \ coc#refresh()
+  inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
   nmap <silent> gd <Plug>(coc-definition)
   nmap <silent> gy <Plug>(coc-type-definition)
   nmap <silent> gi <Plug>(coc-implementation)
--- config/.config/nvim/plugin/nvim-autopairs.lua
+++ config/.config/nvim/plugin/nvim-autopairs.lua
@@ -3,4 +3,19 @@ if (not no_errors) then
   return
 end
 
-autopairs.setup({})
+-- configuration of <CR> behavior in coc-nvim
+-- ref. https://github.com/windwp/nvim-autopairs/wiki/Completion-plugin
+local remap = vim.api.nvim_set_keymap
+autopairs.setup({map_cr=false})
+
+_G.MUtils= {}
+
+MUtils.completion_confirm=function()
+  if vim.fn["coc#pum#visible"]() ~= 0  then
+    return vim.fn["coc#pum#confirm"]()
+  else
+    return npairs.autopairs_cr()
+  end
+end
+
+remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
