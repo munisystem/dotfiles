7dc57089be9565a88531e88d047fe15d78fcecd7
Yuichi Saito <munisystem@gmail.com>
2023-01-13 14:43:14 +0900

    Use lazy.nvim instead of packer.nvim

--- config/.config/nvim/lua/munisystem/plugins.lua
+++ config/.config/nvim/lua/munisystem/plugins.lua
@@ -1,53 +1,55 @@
-local no_errors, packer = pcall(require, "packer")
-if (not no_errors) then
-  print("Packer is not found, please install it. https://github.com/wbthomason/packer.nvim")
-  return
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd [[packadd packer.nvim]]
-
-return packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-
-  use 'navarasu/onedark.nvim'
-  use 'nvim-lua/plenary.nvim'
-
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help' -- show function signatures duaring insertion.
-  use 'onsails/lspkind-nvim' -- use VSCode like icons in lsp
-  use 'L3MON4D3/LuaSnip' -- expand suggestion in nvim-cmp
-  use 'williamboman/mason.nvim' -- lsp installer
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'MunifTanjim/prettier.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use {
+require("lazy").setup({
+ 'navarasu/onedark.nvim',
+ 'nvim-lua/plenary.nvim',
+ 'neovim/nvim-lspconfig',
+ 'hrsh7th/nvim-cmp',
+ 'hrsh7th/cmp-buffer',
+ 'hrsh7th/cmp-nvim-lsp',
+ 'hrsh7th/cmp-nvim-lsp-signature-help', -- show function signatures duaring insertion.
+ 'onsails/lspkind-nvim', -- use VSCode like icons in lsp
+ 'L3MON4D3/LuaSnip', -- expand suggestion in nvim-cmp
+ 'williamboman/mason.nvim', -- lsp installer
+ 'williamboman/mason-lspconfig.nvim',
+ 'MunifTanjim/prettier.nvim',
+ 'jose-elias-alvarez/null-ls.nvim',
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use 'RRethy/nvim-treesitter-endwise'
-  use 'andymass/vim-matchup' -- highlight set of matching texts
-  use {
+    build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  },
+  'RRethy/nvim-treesitter-endwise',
+  'andymass/vim-matchup', -- highlight set of matching texts
+  {
     'kylechui/nvim-surround',
-    tag = "*",
-  }
-  use 'lukas-reineke/indent-blankline.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use {
+    version = "*",
+  },
+  'lukas-reineke/indent-blankline.nvim',
+  'kyazdani42/nvim-web-devicons',
+  {
     'nvim-telescope/telescope.nvim',
-    requires = {{ 'nvim-lua/plenary.nvim' }}
-  }
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'numToStr/Comment.nvim'
-  use {
+    dependencies = {{ 'nvim-lua/plenary.nvim' }}
+  },
+  'nvim-telescope/telescope-file-browser.nvim',
+  'windwp/nvim-autopairs',
+  'numToStr/Comment.nvim',
+  {
     'akinsho/bufferline.nvim',
-    tag = "v2.*",
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-  use 'github/copilot.vim'
-  use 'dinhhuy258/git.nvim'
-  use 'lewis6991/gitsigns.nvim'
-end)
+    version = "v2.*",
+    dependencies = 'kyazdani42/nvim-web-devicons'
+  },
+  'github/copilot.vim',
+  'dinhhuy258/git.nvim',
+  'lewis6991/gitsigns.nvim',
+})
