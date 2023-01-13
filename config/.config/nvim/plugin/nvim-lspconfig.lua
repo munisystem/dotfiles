31b9b224fa6454b1194de400382be6df37a336e2
Yuichi Saito <munisystem@gmail.com>
2023-01-13 14:59:53 +0900

    Don't use deprecated method

--- config/.config/nvim/plugin/nvim-lspconfig.lua
+++ config/.config/nvim/plugin/nvim-lspconfig.lua
@@ -22,7 +22,7 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local util = require('lspconfig/util')
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
