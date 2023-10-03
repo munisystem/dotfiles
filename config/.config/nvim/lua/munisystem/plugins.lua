54119ca7cc2e47ab0e68b1a5f1d9ef05782052ce
Yuichi Saito <munisystem@gmail.com>
2023-10-03 11:35:02 +0900

    Use legacy plugin

--- config/.config/nvim/lua/munisystem/plugins.lua
+++ config/.config/nvim/lua/munisystem/plugins.lua
@@ -21,9 +21,12 @@ require("lazy").setup({
   "hrsh7th/cmp-nvim-lsp-signature-help", -- show function signatures duaring insertion.
   "hrsh7th/cmp-path",
   "onsails/lspkind-nvim",               -- use VSCode like icons in lsp
-  "j-hui/fidget.nvim",                  -- lsp loading indicator
-  "L3MON4D3/LuaSnip",                   -- expand suggestion in nvim-cmp
-  "williamboman/mason.nvim",            -- lsp installer
+  {
+    "j-hui/fidget.nvim",                -- lsp loading indicator
+    tag = "legacy",
+  },
+  "L3MON4D3/LuaSnip",       -- expand suggestion in nvim-cmp
+  "williamboman/mason.nvim", -- lsp installer
   "williamboman/mason-lspconfig.nvim",
   "MunifTanjim/prettier.nvim",
   {
