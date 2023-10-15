726c2bdcb48100dc00780a13df1b6d7b720e40a9
Yuichi Saito <munisystem@gmail.com>
2023-10-15 13:24:02 +0900

    Fix broken finder of Lspsaga

--- config/.config/nvim/plugin/lspsaga.lua
+++ config/.config/nvim/plugin/lspsaga.lua
@@ -9,7 +9,7 @@ lspsaga.setup({
     title = true,
   },
   diagnostic = {
-    swhow_code_action = false,
+    show_code_action = false,
   },
   symbol_in_winbar = {
     enable = false,
@@ -22,7 +22,7 @@ lspsaga.setup({
 
 local keymap = vim.keymap.set
 keymap("n", "gl", "<cmd>Lspsaga show_line_diagnostics<CR>")
-keymap("n", "gd", "<cmd>Lspsaga lsp_finder<CR>")
+keymap("n", "gd", "<cmd>Lspsaga finder<CR>")
 keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
 keymap("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
 keymap("n", "gh", "<cmd>Lspsaga hover_doc<CR>")
