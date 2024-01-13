077707c20c27441370d8945ef6d3cba8c493d40f
Yuichi Saito <munisystem@gmail.com>
2024-01-13 14:51:59 +0900

    Add karabiner configs

--- itamae/cookbooks/tmux/default.rb
+++ itamae/cookbooks/tmux/default.rb
@@ -6,7 +6,4 @@ git "#{ENV["HOME"]}/.tmux/plugins/tpm" do
   repository 'https://github.com/tmux-plugins/tpm'
 end
 
-execute "#{ENV["HOME"]}/.tmux/plugins/tpm/bin/install_plugins" do
-  subscribes :run, "link[#{ENV['HOME']}/.config/tmux/tmux.conf]"
-  action :nothing
-end
+execute "#{ENV["HOME"]}/.tmux/plugins/tpm/bin/install_plugins"
